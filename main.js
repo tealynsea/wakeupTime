1f2d07f900e09de235547ef26b945bbc20ff0a05
Teri <tealynsea@hotmail.com>
2014-07-01 14:08:55 -0600

    need to add a clock face

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
 $(document).on('ready', function() {
   
+
+
+var outerShell = $('<div class="outer-shell"></div>');
+var innerShell = $('<div class="inner-shell"></div>'); 
+var timeLabels = $('<div class="time-labels"><br>PM<br><br><br><br>AUTO<br></div>');
+var clockScreen = $('<div class="clock-screen"></div>');
+var clockText = $('<div class ="clock-text"></div>');
+var amLablFreq = $('<div class ="am-lable"></div>');
+var fmLablFreq = $('<div class ="fm-lable"></div>');
+
+$('.container').append(outerShell);
+
+outerShell.append(innerShell);
+innerShell.append(timeLabels);
+timeLabels.append(clockScreen);
+clockScreen.append(clockText);
+clockText.append(amLablFreq);
+amLablFreq.append(fmLablFreq);
+
+
+
+
+
+
 });
