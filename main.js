f8d6ed6967f9f7680ad6d252b6631690479d4ae6
Teri <tealynsea@hotmail.com>
2014-07-01 15:52:51 -0600

    layout complete; need to put working clock in

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ var outerShell = $('<div class="outer-shell"></div>');
 var innerShell = $('<div class="inner-shell"></div>'); 
 var timeLabels = $('<div class="time-labels"><br>PM<br><br><br><br>AUTO<br></div>');
 var clockScreen = $('<div class="clock-screen"></div>');
-var clockText = $('<div class ="clock-text"></div>');
-var amLablFreq = $('<div class ="am-lable"></div>');
-var fmLablFreq = $('<div class ="fm-lable"></div>');
+var clockText = $('<div class ="clock-text">10:00</div>');
+var amLablFreq = $('<div class ="am-lable">AM       53  60 90  110  140  170</div>');
+var fmLablFreq = $('<div class ="fm-lable">FM 88 92 96 101 104 107<div>');
 
 $('.container').append(outerShell);
 
